7d0f50b0cc87920b19724ba04624d9e80cbaac44
Devin Warrick <dev.warr@gmail.com>
2019-10-28 12:59:35 -0500

    Created get request

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db")
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.status(200).json({"response":"Helllllooooooooo?!?!?!?!?!!!"});
+});
+
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then(res => res.status(200).json(res))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({"error":"Sorry, we had an interal error. Time to gut ourselves!"});
+        });
+})
+
+const port = 2019;
+server.listen(port, () => console.log(`\n=== We're up! Port ${port}. ===\n`));
