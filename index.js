a8969c32e9defa29595f9718a4b2b2020aca8d42
Devin Warrick <dev.warr@gmail.com>
2019-10-29 03:02:50 -0500

    Specifications typed out

--- index.js
+++ index.js
@@ -10,15 +10,22 @@ server.get("/", (req, res) => {
 });
 
 server.get("/api/users", (req, res) => {
+
     db.find()
         .then(users => res.status(200).json(users))
         .catch(err => {
             console.log(err)
-            res.status(500).json({"error":"Sorry, we had an interal error. Time to gut ourselves!"});
+            res.status(500).json({"error":"Sorry, we had an interal error. No Users for you. Time to gut ourselves!"});
         });
 })
 
 server.post("/api/users", (req, res) => {
+
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({"errorMessage":"Please provide both a 'name' and a 'bio'. Plzz"});
+        return;
+    }
+
     db.insert(req.body)
         .then(ret => {
             db.findById(ret.id)
@@ -31,25 +38,52 @@ server.post("/api/users", (req, res) => {
 })
 
 server.get("/api/users/:id", (req, res) => {
+
     db.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({"errorMessage":"Did you give us a valid id?"});
+                return;
+            }
             res.status(200).json(user);
         })
-        .catch(err => res.status(500).json({"error":"Sorry, we had an interal error. Time to gut ourselves!"}))
+        .catch(err => res.status(500).json({"error":"Sorry, we had an interal error. No User info this time. Time to gut ourselves!"}))
 });
 
 server.delete("/api/users/:id", (req, res) => {
+
     db.remove(req.params.id)
         .then(deleted => {
+            if (!deleted) {
+                res.status(404).json({"errorMessage":"Did you give us a valid id?"});
+                return;
+            }
             res.status(200).json({"numberOfRemovedRecords":deleted});
         })
-        .catch(err => res.status(500).json({"error":"Sorry, we had an interal error. Time to gut ourselves!"}));
+        .catch(err => res.status(500).json({"error":"Sorry, we had an interal error. The user is not deleted. Time to gut ourselves!"}));
 });
 
 server.put("/api/users/:id", async (req, res) => {
+
+    if (!req.body.name && !req.body.bio) {
+        res.status(400).json({"errorMessage":"Please provide etiher a 'name' and a 'bio' to update. Plzz"});
+        return;
+    };
+    
+    const updatedUser = {};
+    if (!!req.body.name) {
+        updatedUser.name = req.body.name;
+    };
+    if (!!req.body.bio) {
+        updatedUser.bio = req.body.bio;
+    };
+
     try {
-        const success = await db.update(req.params.id, req.body);
-        if (!success) throw new Error("update unsuccessful");
+        const success = await db.update(req.params.id, updatedUser);
+        if (!success) {
+            res.status(404).json({"errorMessage":"Did you give us a valid id?"});
+            return;
+        }
         const updated = await db.findById(req.params.id);
         res.status(200).json(updated);
     } catch (e) {
